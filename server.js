224d0bf8b40964dd657b35be632da74287213028
GulpinL <53393464+GulpinL@users.noreply.github.com>
2022-04-13 15:52:32 +0700

    lib cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import db from './config/db/connect.js';
 import route from './routes/index.js';
 import dotenv from 'dotenv';
+import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: true,
 }));
+app.use(cors())
 dotenv.config();
 db.connect();
 
